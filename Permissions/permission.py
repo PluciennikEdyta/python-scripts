5550a528ad9ea1dd0d884a60c6535afa0da5f81e
Pluciennik Edyta <edyta.pluciennik@tieto.com>
2018-06-20 22:02:13 +0200

    feature: oop introduction

--- Permissions/permission.py
+++ Permissions/permission.py
@@ -1,12 +1,114 @@
 #!/usr/bin/python
+
+import os
 import subprocess
+
 import xlsxwriter
+
+
+class CommandNotArtifactableException(RuntimeError):
+    pass
+
+
+class NotEnoughCommandParams(RuntimeError):
+    pass
+
+
+class BaseCommand(object):
+    should_gather_artifacts = False
+    should_return_value = False
+    artifacts_path = ''
+    command = ''
+    command_params = []
+
+    def prepare(self, *args, **kwargs):
+        for param in self.command_params:
+            if param not in kwargs:
+                raise NotEnoughCommandParams("Parameter {} should be passed.".format(param))
+        self.command = self.command.format(**kwargs)
+
+    def execute(self, *args, **kwargs):
+        # TODO: introduce live streaming output
+        output = subprocess.check_output(self.command.split())
+        if self.should_return_value:
+            return output
+
+    def collect_artifacts(self, *args, **kwargs):
+        if not self.should_gather_artifacts:
+            raise CommandNotArtifactableException("Command is not set to be artifactable.")
+
+    def clean(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class ListPermissionsCommand(BaseCommand):
+    should_gather_artifacts = True
+    should_return_value = True
+    artifacts_path = os.getcwd()
+    command = 'adb shell pm list permissions {package_name}'
+    command_params = ['package_name', ]
+
+
+class DumpsStateCommand(BaseCommand):
+    command = 'adb shell dumpstate > state.logs'
+
+
+class KillAppCommand(BaseCommand):
+    command = 'adb -d shell am force-stop {package_name}'
+    command_params = ['package_name', ]
+
+
+class TakeScreenshotCommand(BaseCommand):
+    command = 'adb shell screencap /sdcard/screen.png'
+
+
+class SaveOnComputerCommand(BaseCommand):
+    command = 'adb pull /sdcard/screen.png'
+
+
+class InstallAppCommand(BaseCommand):
+    command = 'adb install {package_name}'
+    command_params = ['package_name', ]
+
+
+class UninstallAppCommand(BaseCommand):
+    command = 'adb unistall {package_name}'
+    command_params = ['package_name', ]
+
+
+class UpgradeAppCommand(BaseCommand):
+    command = 'adb install -r {package_name}'
+    command_params = ['package_name', ]
+
+
+class CountedPingCommand(BaseCommand):
+    """
+    Example of usage:
+
+    p = CountedPingCommand()
+    p.prepare(host='www.google.com', count=3)
+    print p.execute()
+    """
+
+    command = 'ping {host} -c {count}'
+    command_params = ['host', 'count', ]
+    should_return_value = True
+
+
+class CommandManager(object):
+    # TODO
+    pass
 
 
 COMMANDS = {
     'list_permissions': 'adb shell pm list permissions {package_name}',
     'dumps_state': 'adb shell dumpstate > state.logs',
     'kill_app': 'adb -d shell am force-stop {package_name}',
+    'take_screenshot': 'adb shell screencap /sdcard/screen.png',
+    'save_on_computer': 'adb pull /sdcard/screen.png',
+    'install_app': 'adb install {package_name}',
+    'uninstall_app': 'adb unistall {package_name}',
+    'upgrade_app': 'adb install -r {package_name}',
 }
 OMIT_DURING_REGULAR_FLOW = ['kill_app', ]
 
